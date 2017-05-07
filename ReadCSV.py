ec1b4316376d5e6f7ac23235ab1e6a28e378b44d
zakton <anton.hassan@gmail.com>
2017-05-07 07:36:09 +0700

    Completed ReadCSV: to read, to get unique, to search

--- ReadCSV.py
+++ ReadCSV.py
@@ -4,6 +4,7 @@
 
 import unicodecsv
 
+# --- 1 ---
 # Reading a csv
 # File name: actors.csv. Key: actor's name. Values: birthdate; birth place city, state and country;
 # ... number of facebook likes.
@@ -14,9 +15,11 @@ with open('actors.csv', 'rb') as f:
 
 print(actors_list)
 
+# --- 2 ---
 # Reading a csv with duplicate keys
+# Use of set come up with unique items
 # File name: actor_movie.csv
-# How many unique actors are there in the table?
+# How many unique actors are there in the csv file?
 
 with open('actor_movie.csv', 'rb') as f:
     reader = unicodecsv.DictReader(f)
@@ -32,14 +35,17 @@ for a in actor_movie_list:
 print(actor_set)
 print(len(actor_set))
 
-# How many movies is Ryan Gosling starring in?
-actor = 'Ryan Gosling'
+# --- 3 ---
+#  What movies does Ryan Gosling star in? How many?
+# Again use set to keep unique movie titles
+
+searched_actor = 'Ryan Gosling'
 movie_set = set()
 for a in actor_movie_list:
-    if a['actor_name'] == actor:
+    if a['actor_name'] == searched_actor:
         movie_set.add(a['movie_title'])
 
-print('Ryan Gosling stars in the following movies: ')
+print('Ryan Gosling stars in these movies: ')
 print(movie_set)
 print(len(movie_set))
 
